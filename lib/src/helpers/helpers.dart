b31bcbbe7705d0053b47809eeccfb0f4faf97745
Jens Lam <jesterlynn1986@gmail.com>
2022-08-14 17:48:21 -0700

    First commit

---
+++ lib/src/helpers/helpers.dart
@@ -0,0 +1,46 @@
+
+
+List<String> getLyrics() {
+
+  return [
+    'Well am I making haste or could it be haste is making me',
+    'What\'s time but a thing to kill or keep or buy or lose or live in',
+    'I gotta go faster',
+    'Keep up the pace',
+    'Just to stay in the human race',
+    'I could go supersonic',
+    'The problem\'s chronic',
+    'Tell me does life exist beyond it'
+    'When I need to sate',
+    'I just accelerate',
+    'Into oblivion',
+    'Into oblivi yah yah yah yah yah yah yan',
+    'Now here I go again',
+    'Everything is alien',
+    'How does it feel to be outstripped by the pace of cultural change',
+    'My deeds are senseless',
+    'And rendered meaningless',
+    'When measured in that vein',
+    'I could go supersonic',
+    'The problem\'s chronic',
+    'Tell me does life exist beyond it',
+    'When I need to sate',
+    'I just accelerate into oblivion',
+    'Into oblivion',
+    'I won\'t lie',
+    'It\'s exciting',
+    'When I try',
+    'To decide things',
+    'I just want to live',
+    'Decently',
+    'Meaningfully',
+    'I\'m in misery',
+    'I could I go supersonic',
+    'The problem\'s chronic',
+    'Tell me does life exist beyond it',
+    'When I need to sate',
+    'I just accelerate into oblivion',
+    'Into oblivi yah yah yah yah yah yah yan'
+  ];
+
+}
---
+++ lib/src/screens/screens.dart
@@ -0,0 +1,1 @@
+export 'package:music_player/src/screens/music_player_screen.dart';
